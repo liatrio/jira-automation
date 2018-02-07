cbe0eb227089482943add2e9e4e3fd9650094388
Justin Bankes <jbankes1@gmail.com>
2018-02-07 13:52:56 -0800

    Base functionality created.

--- blog_ticket.py
+++ blog_ticket.py
@@ -1,13 +1,18 @@
+# file: blog_ticket.py
+# author: Justin Bankes
 import os
 import sys
 import getpass
 from jira import JIRA
 
+
 def init_jira():
-    user_name = input('Username: ')
-    password = getpass.getpass('Password: ') 
-    jira = JIRA('https://liatrio.atlassian.net', basic_auth=(user_name, password))
+    jira_server = input('JIRA URL: ')
+    user_name = input('username: ')
+    password = getpass.getpass('password: ') 
+    jira = JIRA(jira_server, basic_auth=(user_name, password))
     return jira
+
 
 def populate_users(file_name):
     user_list = []
@@ -20,14 +25,18 @@ def populate_users(file_name):
         sys.exit()
 
 
-def create_blog(user, jira):
-    pass
-
+def create_blog(jira, user, month):
+    ticket_summary = f"{month} blog - {user}"
+    ticket_desc = "_Please update with information about your blog._\n*Blog Title:*\n*Topic Desciption:*"
+    new_issue = jira.create_issue(project='CHICO', summary=ticket_summary, 
+            description=ticket_desc, issuetype={'name':'Task'}, 
+            assignee={'name': user})
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 3:
         file_name = sys.argv[1]
+        month = sys.argv[2]
     else:
         print("Please enter a file name to use, try again...")
         sys.exit(1)
@@ -36,5 +45,5 @@ if __name__ == '__main__':
     jira = init_jira()
 
     for user in user_list:
-        create_blog(user, jira)
-    
+        create_blog(jira, user, month)
+ 
